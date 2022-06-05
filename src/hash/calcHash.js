147db7fe20aec9f7ea5889a58cfb3254ac8c4b99
Tatsiana Slapik <slapik.tatiana@gmail.com>
2022-06-05 22:39:15 +0300

    feat: implement function that calculates SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,12 @@
+import { createHash } from "node:crypto";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const pathHashFile = "src/hash/files/fileToCalculateHashFor.txt";
+  const typeHash = "sha256";
+  const typeReturned = "hex";
+
+  return console.log(
+    createHash(typeHash).update(pathHashFile).digest(typeReturned)
+  );
 };
+calculateHash();
