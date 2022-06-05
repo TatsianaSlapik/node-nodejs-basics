2c2f42cb3f9a761c7fa59619cc572a4c1a8ca4b6
Tatsiana Slapik <slapik.tatiana@gmail.com>
2022-06-05 22:38:33 +0300

    feat: implement functions in 'Command line interface' block

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
+import { env } from "node:process";
+
 export const parseEnv = () => {
-    // Write your code here 
+  const variablesPrefix = "RSS_";
+
+  return Object.keys(env)
+    .filter((val) => val.startsWith(variablesPrefix))
+    .forEach((val) => {
+      console.log(`${val}=${process.env[val]};`);
+    });
 };
+parseEnv();
