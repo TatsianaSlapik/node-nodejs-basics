f95a450e98682c75efa27262772c631fb2ab99ed
Tatsiana Slapik <slapik.tatiana@gmail.com>
2022-06-05 23:36:43 +0300

    feat: implement function in 'Child Processes' block

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,28 @@
+import { spawn } from "node:child_process";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const nameFolder = "files";
+  const nameFile = "script.js";
+
+  const childProcess = spawn("node", [
+    path.join(
+      path.dirname(fileURLToPath(import.meta.url)),
+      nameFolder,
+      nameFile
+    ),
+    ...args.slice(2),
+  ]);
+
+  process.stdin.on("data", (data) => {
+    console.log("it receive input from master process");
+    childProcess.stdin.write(data);
+  });
+
+  childProcess.stdout.on("data", (data) => {
+    process.stdout.write(`Received from the child process: ${data} \n ${data}`);
+  });
 };
+
+spawnChildProcess(process.argv);
